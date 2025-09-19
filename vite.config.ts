1dfe19254859607477876c3774cc32029bb1cf81
Wasuu786 <wasimpathan2406@gmail.com>
2025-09-20 04:20:38 +0530

    hg

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Wasim-pathan-/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
