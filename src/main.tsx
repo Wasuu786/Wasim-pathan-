06c74ff69468987537a37bf26786759a064bf7ce
Wasuu786 <wasimpathan2406@gmail.com>
2025-09-20 05:52:49 +0530

    rdr

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
   
